db0e6f6bed7b9677365cf9e812beb51c48f44798
Rick Thomas <thomas.richard.l@gmail.com>
2024-12-06 04:54:53 -0500

    Added endpoints to get confirmed appointments, added logic to ensure providers don't get double-booked

--- python3_12/ReservationAPI/main.py
+++ python3_12/ReservationAPI/main.py
@@ -3,7 +3,7 @@ import uuid
 
 import uvicorn
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from typing import Annotated, Sequence
 from uuid import UUID
 
@@ -80,8 +80,18 @@ async def get_provider(provider_id: uuid.UUID, session: SessionDep) -> Provider:
     return provider
 
 
+@app.get('/providers/{provider_id}/appointments/')
+async def get_provider_appointments(provider_id: UUID, session: SessionDep) -> Sequence[Appointment]:
+    appointments = session.exec(select(Appointment)
+                                .where(Appointment.provider_id == provider_id)
+                                .where(Appointment.is_confirmed == True)).all()
+    return appointments
+
+
 @app.get('/providers/{provider_id}/availability/')
 async def get_provider_availability(provider_id: uuid.UUID, session: SessionDep) -> list[datetime]:
+    provider_appointments = await get_provider_appointments(provider_id, session)
+    appointment_times = [appointment.appointment_time for appointment in provider_appointments]
     provider_availability = session.exec(select(Availability).where(Availability.provider_id == provider_id)).all()
     available_appointments = []
     for available_window in provider_availability:
@@ -90,7 +100,8 @@ async def get_provider_availability(provider_id: uuid.UUID, session: SessionDep)
         delta = timedelta(minutes=APPOINTMENT_LENGTH)
         current = start
         while current < end:
-            available_appointments.append(current)
+            if current not in appointment_times:
+                available_appointments.append(current)
             current += delta
     return available_appointments
 
@@ -100,7 +111,7 @@ async def add_provider_availability(
         provider_id: uuid.UUID,
         available_from: Annotated[datetime, Body()],
         available_to: Annotated[datetime, Body()],
-        session: SessionDep) -> Sequence[Availability]:
+        session: SessionDep) -> list[datetime]:
     provider = await get_provider(provider_id, session)
     availability = Availability(
         provider_id=provider_id,
@@ -141,6 +152,14 @@ async def get_client(client_id: uuid.UUID, session: SessionDep) -> Client:
     return client
 
 
+@app.get('/clients/{client_id}/appointments/')
+async def get_client_appointments(client_id: UUID, session: SessionDep) -> Sequence[Appointment]:
+    appointments = session.exec(select(Appointment)
+                                .where(Appointment.client_id == client_id)
+                                .where(Appointment.is_confirmed == True)).all()
+    return appointments
+
+
 @app.post('/appointments/reserve')
 async def reserve_appointment(
         client_id: Annotated[UUID, Body()],
@@ -157,6 +176,8 @@ async def reserve_appointment(
                               provider_id=provider_id,
                               appointment_time=appointment_time)
     db_appointment = Appointment.model_validate(appointment)
+    if time < (datetime.now() + timedelta(hours=24)):
+        raise HTTPException(status_code=400, detail='Appointments must be made at least 24 hours in advance')
     if time not in available_appointments:
         raise HTTPException(status_code=400, detail='Time requested is not available, please see provider availability')
     else:
@@ -164,6 +185,32 @@ async def reserve_appointment(
         session.commit()
         session.refresh(db_appointment)
         return db_appointment
+
+
+@app.get('/appointments/{appointment_id}/')
+async def get_appointment(appointment_id: UUID, session: SessionDep) -> Appointment:
+    appointment = session.get(Appointment, appointment_id)
+    if not appointment:
+        raise HTTPException(status_code=404, detail='Appointment not found')
+    return appointment
+
+
+@app.post('/appointments/{appointment_id}/confirm/')
+async def confirm_appointment(
+        appointment_id: UUID,
+        session: SessionDep
+) -> Appointment:
+    appointment = await get_appointment(appointment_id, session)
+    current_availability = await get_provider_availability(appointment.provider_id, session)
+    if appointment.appointment_time not in current_availability:
+        raise HTTPException(status_code=400, detail='Appointment time already taken - please reserve a new appointment')
+    if not appointment.is_confirmed and appointment.created_on < (datetime.now() - timedelta(minutes=30)):
+        raise HTTPException(status_code=400, detail='Appointments must be confirmed within 30 minutes of being reserved')
+    appointment.is_confirmed = True
+    session.add(appointment)
+    session.commit()
+    session.refresh(appointment)
+    return appointment
 
 
 if __name__ == "__main__":
